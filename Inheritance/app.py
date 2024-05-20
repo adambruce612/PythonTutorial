96b04ca5507654aee6ce0a9ca882526a0ee61187
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2024-05-20 18:00:55 -0500

    Add files via upload

---
+++ Inheritance/app.py
@@ -0,0 +1,9 @@
+from Chef import Chef
+from ChineseChef import ChineseChef
+
+myChef = Chef()
+myChef.make_chicken()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_special_dish()
+myChineseChef.make_fried_Rice()
